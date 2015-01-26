2e127138b2a5bd27a5b8f7082213cb1ec58a1810
geoyws <geoyws@gmail.com>
2015-01-26 11:00:05 +0800

    alamaks

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
 });
 
 var server = app.listen('port', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  //var host = server.address().address;
+  //var port = server.address().port;
+  console.log('Listening to requests...');
+  //console.log('Example app listening at http://%s:%s', host, port);
 });
