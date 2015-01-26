84926d72e2b3aca08df4c6a0e82b18b3bbdfeb63
geoyws <geoyws@gmail.com>
2015-01-26 10:39:50 +0800

    finally

--- canvas/js/core.js
+++ canvas/js/core.js
@@ -1,12 +1,15 @@
-$(function () {
-  FB.init({
-    appId: 1027803050567274,
-    frictionlessRequests: true,
-    status: true,
-    version: 'v2.1'
-  });
+function login () {
+  $(function () {
+    FB.init({
+      appId: 1027803050567274,
+      frictionlessRequests: true,
+      status: true,
+      xfbml: false,
+      version: 'v2.0'
+    });
 
-  FB.Event.subscribe('auth.authResponseChange', onAuthResponseChange);
-  FB.Event.subscribe('auth.statusChange', onStatusChange);
-});
+    FB.Event.subscribe('auth.authResponseChange', onAuthResponseChange);
+    FB.Event.subscribe('auth.statusChange', onStatusChange);
+  });
+}
 
