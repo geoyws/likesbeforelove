560248306c9b31ffcf482a54d3402aaf29e06c88
geoyws <geoyws@gmail.com>
2015-01-26 10:53:41 +0800

    alrighty try again

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res) {
   res.send('Likesbeforelove by Josh Lim');
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen('port', function () {
   var host = server.address().address;
   var port = server.address().port;
 
